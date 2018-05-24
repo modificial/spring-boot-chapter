59aad13f6f038069415bacef98c9aeb29a82f537
modificial <1556103759@qq.com>
2018-05-24 23:58:16 +0800

    spring-boot第五篇

---
+++ Chapter5/src/main/java/com/modificial/Chapter5Application.java
@@ -0,0 +1,14 @@
+package com.modificial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Chapter5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter5Application.class, args);
+	}
+}
